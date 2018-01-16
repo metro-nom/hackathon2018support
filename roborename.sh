b61bbcedaaca762fe93771ba18f780a47084bb2f
Thomas Stroeder <thomas.stroeder@metrosystems.net>
2018-01-16 20:48:42 +0100

    robot renaming script

---
+++ roborename.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mv $1/src/main/java/de/metro/robocode/TestBot.java $1/src/main/java/de/metro/robocode/$2.java
+mv $1/src/main/resources/de/metro/robocode/TestBot.properties $1/src/main/resources/de/metro/robocode/$2.properties
+sed -i -e "s/TestBot/$2/g" $1/src/main/java/de/metro/robocode/$2.java
+sed -i -e "s/TestBot/$2/g" $1/src/main/resources/de/metro/robocode/$2.properties
+
