bf5873998388a844946d973cba20d141cc2b025e
andrei.ciobanu01 <andrei.ciobanu01@metrosystems.net>
2018-01-18 15:42:00 +0200

    add command to replace botname for dev purpose

--- roborename.sh
+++ roborename.sh
@@ -4,4 +4,5 @@ mv $1/src/main/resources/de/metro/robocode/TestBot.properties $1/src/main/resour
 sed -i -e "s/TestBot/$2/g" $1/src/main/java/de/metro/robocode/$2.java
 sed -i -e "s/TestBot/$2/g" $1/src/main/resources/de/metro/robocode/$2.properties
 sed -i -e "s/TestBot/$2/g" $1/pom.xml
+sed -i -e "s/TestBot/$2/g" $1/misc/battles.battle
 
